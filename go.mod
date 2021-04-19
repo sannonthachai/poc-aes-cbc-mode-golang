130d1ecb8856ceab1a511662bd596e8ed8c7e951
Chai Sannonthachai <chai@finnomena.com>
2021-04-19 17:48:18 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sannonthachai/poc-aes
+
+go 1.15
+
+require (
+	github.com/labstack/echo/v4 v4.2.1
+	github.com/mergermarket/go-pkcs7 v0.0.0-20170926155232-153b18ea13c9
+)
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/mergermarket/go-pkcs7"
+)
+
+// Cipher key must be 32 chars long because block size is 16 bytes
+const SECRET = "abcdefghijklmnopqrstuvwxyz012345"
+const IV = "1234567890123456"
+
+type Credential struct {
+	TranId   string `json:"tran_id"`
+	QrCode   string `json:"qr_code"`
+	IdCard   string `json:"id_card"`
+	MobileNo string `json:"mobile_no,omitempty"`
+}
+
+func main() {
+	mockJson, _ := ioutil.ReadFile("mock.json")
+	compactedBuffer := new(bytes.Buffer)
+	err := json.Compact(compactedBuffer, mockJson)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	plainText := compactedBuffer.String()
+	fmt.Println("plainText", plainText)
+
+	encrypted, err := Encrypt(plainText)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Failed to encrypt: %s - %s", plainText, err.Error()))
+	}
+
+	decrypted, err := Decrypt(encrypted)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Failed to decrypt: %s - %s", plainText, err.Error()))
+	}
+
+	fmt.Println("encrypt", encrypted)
+	fmt.Println("decrypted", decrypted)
+
+	e := echo.New()
+	e.POST("/", func(c echo.Context) error {
+		var bodyBytes []byte
+		if c.Request().Body != nil {
+			bodyBytes, _ = ioutil.ReadAll(c.Request().Body)
+		}
+
+		fmt.Println(string(bodyBytes))
+
+		de, err := Decrypt(string(bodyBytes))
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Failed to decrypt: %s - %s", plainText, err.Error()))
+		}
+
+		test := Credential{}
+		json.Unmarshal([]byte(de), &test)
+
+		c.Set("credential-test", test)
+		cre := c.Get("credential-test").(Credential)
+		fmt.Println(cre)
+
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Encrypt encrypts plain text string into cipher text string
+func Encrypt(unencrypted string) (string, error) {
+	key := []byte(SECRET)
+	plainText := []byte(unencrypted)
+
+	plainText, err := pkcs7.Pad(plainText, aes.BlockSize)
+	if err != nil {
+		return "", fmt.Errorf(`plainText: "%s" has error`, plainText)
+	}
+	if len(plainText)%aes.BlockSize != 0 {
+		err := fmt.Errorf(`plainText: "%s" has the wrong block size`, plainText)
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, len(plainText))
+	iv := []byte(IV)
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText, plainText)
+
+	return fmt.Sprintf("%x", cipherText), nil
+}
+
+// Decrypt decrypts cipher text string into plain text string
+func Decrypt(encrypted string) (string, error) {
+	key := []byte(SECRET)
+	cipherText, _ := hex.DecodeString(encrypted)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		panic("cipherText too short")
+	}
+	iv := []byte(IV)
+	if len(cipherText)%aes.BlockSize != 0 {
+		panic("cipherText is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(cipherText, cipherText)
+
+	cipherText, _ = pkcs7.Unpad(cipherText, aes.BlockSize)
+	return fmt.Sprintf("%s", cipherText), nil
+}
